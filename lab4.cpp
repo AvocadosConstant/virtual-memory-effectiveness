f1435e31ccd5159901a9b654537cfce05bbdd91a
Tim <thung1@binghamton.edu>
2016-03-15 12:43:06 -0400

    Implement command line args for lab4

--- lab4.cpp
+++ lab4.cpp
@@ -6,7 +6,15 @@
 #include <vector>
 #include <ctime>
 
-int main(){
+int main(int argc, char* argv[]){
+    if(argc != 4) {
+        std::perror("Unexpected number of arguments!");
+        return 0;
+    }
+    int numProcs = atoi(argv[1]);
+    int addressSize = atoi(argv[2]);
+    int repeatPercent = atoi(argv[3]);
 
-  return 0;
+    printf("%d processes\n%d address size\n%d\% repetition\n", numProcs, addressSize, repeatPercent);
+    return 0;
 }
